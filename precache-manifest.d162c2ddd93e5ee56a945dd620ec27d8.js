8f0c7e22a0d072d2706f0dbca6150392a3a8b7d1
David Hartsough <hartsoughdavid@gmail.com>
2018-11-19 19:38:22 -0700

    Updates

---
+++ precache-manifest.d162c2ddd93e5ee56a945dd620ec27d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8c89fa2d686ba361434",
+    "url": "/static/js/main.d8c89fa2.chunk.js"
+  },
+  {
+    "revision": "79727ee0dfca2e4331c6",
+    "url": "/static/js/1.79727ee0.chunk.js"
+  },
+  {
+    "revision": "d8c89fa2d686ba361434",
+    "url": "/static/css/main.ff1e2868.chunk.css"
+  },
+  {
+    "revision": "1bd313b659e4c1545384ee0970b97eff",
+    "url": "/index.html"
+  }
+];
