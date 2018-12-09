800cd30d3f7862c2d94ead9b0e03ee58f2159797
David Hartsough <hartsoughdavid@gmail.com>
2018-12-09 15:45:09 -0700

    Updates

--- precache-manifest.ce822fad9125d9b1143cf108b3bceb9e.js
+++ precache-manifest.ce822fad9125d9b1143cf108b3bceb9e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "55466d7ba25874aae8ee",
-    "url": "/static/js/main.55466d7b.chunk.js"
+    "revision": "17922b79384a5af6197d",
+    "url": "/static/js/main.17922b79.chunk.js"
   },
   {
     "revision": "df1b76a2958bc2bef6ec",
     "url": "/static/js/1.df1b76a2.chunk.js"
   },
   {
-    "revision": "55466d7ba25874aae8ee",
+    "revision": "17922b79384a5af6197d",
     "url": "/static/css/main.7d5f48ba.chunk.css"
   },
   {
-    "revision": "36037a377625d5858fa7f9759e06a127",
+    "revision": "21765cc12b75ad10289349affe609be5",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bac77dcbdc8083563ad72571bada612b.js"
+  "/precache-manifest.ce822fad9125d9b1143cf108b3bceb9e.js"
 );
 
 workbox.clientsClaim();
